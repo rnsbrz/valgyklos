33c92fe86bd558c4fa5f5494e6dd08af649899a8
rnsbrz <167898944+rnsbrz@users.noreply.github.com>
2024-06-27 09:44:10 +0300

    added config and authority related code

---
+++ src/main/java/com/spring/valgyklos/config/SpringConfig.java
@@ -0,0 +1,31 @@
+package com.spring.valgyklos.config;
+
+import com.spring.valgyklos.repositories.UserRepository;
+import com.spring.valgyklos.service.UserDetailsServiceImpl;
+import com.spring.valgyklos.service.UserService;
+import com.spring.valgyklos.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SpringConfig {
+
+    @Autowired
+    public SpringConfig(UserRepository userRepository) {
+    }
+
+    @Bean
+    @Qualifier("UserDetailsService")
+    public UserDetailsService getuserDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    @Qualifier("UserService")
+    public UserService getUserService(){
+        return new UserServiceImpl();
+    }
+}
